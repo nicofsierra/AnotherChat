4fcc27730c3c3bc82565b20126087b32c37e67d8
nsierra <nicofsierra@gmail.com>
2021-11-01 10:52:27 -0300

    se divide pantalla con BoderPane y acceso a Sala de chat

--- src/unlam/prograva/taller/graphics/Principal.java
+++ src/unlam/prograva/taller/graphics/Principal.java
@@ -12,8 +12,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -28,20 +33,45 @@ public class Principal extends Application {
 
 	@Override
 	public void start(Stage stage) {
-
-		salas.CrearSala("nico");
-
-		Pane pane = new Pane();
-
+		
+		
+		BorderPane borderPane = new BorderPane();
+		
 		// Label for education
-		Label label = new Label("Educational qualification:");
+		Label label = new Label("Salas Activas:");
 		Font font = Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 12);
 		label.setFont(font);
 		// list View for educational qualification
 		ObservableList<String> names = FXCollections.observableArrayList(salas.getSalas());
 		ListView<String> listView = new ListView<String>(names);
-		listView.setMaxSize(200, 160);
+		listView.setMaxSize(900, 500);
 
+		listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	        @Override
+	        public void handle(MouseEvent mouseEvent) {
+
+	            if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+	                if (mouseEvent.getClickCount() == 2) {
+	                    String item = listView.getSelectionModel()
+	                            .getSelectedItem();
+	                    if (item != null) {
+	                        StackPane pane = new StackPane();
+	                        Scene scene = new Scene(pane);
+	                        Stage stage = new Stage();
+	                        stage.setScene(scene);
+
+	                        pane.getChildren().add(
+	                                new TextField(item));
+
+	                        stage.show();
+	                    }
+
+	                }
+	            }
+	        }
+	    });
+		
+		
 		Button okBtn = new Button();
 		okBtn.setText("Crear Sala");
 		okBtn.setOnAction(new EventHandler<ActionEvent>() {
@@ -66,11 +96,21 @@ public class Principal extends Application {
 		layout.setPadding(new Insets(5, 5, 5, 50));
 		layout.getChildren().addAll(label, listView);
 		layout.setStyle("-fx-background-color: BEIGE");
-		layout.getChildren().addAll(okBtn, salir);
-
+		
+		borderPane.setCenter(layout);
+		
+		
+		HBox layout2 = new HBox(10);
+		layout2.setPadding(new Insets(5, 5, 5, 50));
+		layout2.getChildren().addAll(okBtn, salir);
+		
+		borderPane.setBottom(layout2);
+		
 		// Setting the stage
-		Scene scene = new Scene(layout, 595, 200);
-		stage.setTitle("List View Example");
+		Scene scene = new Scene(borderPane, 1024, 780);
+
+		
+		stage.setTitle("Another Chat - Lobby");
 
 		stage.setScene(scene);
 		stage.show();
